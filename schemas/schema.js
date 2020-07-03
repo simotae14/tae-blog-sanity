c81a3fb84cb52d54eac944dc7366812ac056d27c
Simone <startae14@gmail.com>
2020-07-04 00:04:38 +0200

    create my first data type in the schema of sanity studio

--- schemas/schema.js
+++ schemas/schema.js
@@ -12,5 +12,27 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    {
+      name: "blog",
+      type: "document",
+      title: "Blog",
+      fields: [
+        {
+          name: "title",
+          type: "string",
+          title: "Title"
+        },
+        {
+          name: "subtitle",
+          type: "string",
+          title: "Subtitle"
+        },
+        {
+          name: "slug",
+          type: "slug",
+          title: "Slug"
+        }
+      ]
+    }
   ])
 })
